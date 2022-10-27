082c3a4875e5f96ca1e76aa74930573aacc3fa92
hipetech <64170764+hipetech@users.noreply.github.com>
2022-10-27 23:09:16 +0300

    fixed task service

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -20,7 +20,8 @@ export class TaskService {
     this.logger.log("using create");
     const taskResult = await this.taskModel.create(dto);
     const subject = await this.subjectModel.findById(dto.subject);
-    await this.subjectModel.findOneAndUpdate(dto.subject, { tasks: [...subject.tasks, taskResult._id] });
+
+    await this.subjectModel.findByIdAndUpdate(dto.subject, { tasks: [...subject.tasks, taskResult._id] });
     return taskResult;
   }
 
